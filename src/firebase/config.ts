7c5cc363ff86fe71822bff9cd81b06d05a2d5d17
Adel Benmaza <adelbenmaza8@gmail.com>
2026-03-15 22:52:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1453866434-90eea",
+  "appId": "1:815568760723:web:1c628c9a314a451107c266",
+  "apiKey": "AIzaSyDE70AgP2TkLqn6i5r3s-lmL1wyeJD6VPw",
+  "authDomain": "studio-1453866434-90eea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "815568760723"
+};
